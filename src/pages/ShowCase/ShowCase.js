c4aacf208983d9e2e7b91243f5746b64648f16a1
juandrepanther <janis@innergang.com>
2021-12-04 20:09:44 +0200

    created showcase inputs

--- src/pages/ShowCase/ShowCase.js
+++ src/pages/ShowCase/ShowCase.js
@@ -1,22 +1,35 @@
 import React from 'react'
+import './ShowCase.css'
 
 function ShowCase() {
  return (
-  <div className='showcase-form card'>
-   <h2>Request a Demo</h2>
-   <form action=''>
-    <div className='form-control'>
-     <input type='text' name='name' placeholder='Name' required />
+  <section className='showcase'>
+   <div className='container grid'>
+    <div className='showcase-text'>
+     <h1>Comfortable Deploy Services</h1>
+     <p>
+      You gonna enjoy this and never forged. Awesome products will make your
+      deployment much easier than before! Fill the form and get our feedback
+      within never!
+     </p>
     </div>
-    <div className='form-control'>
-     <input type='text' name='company' placeholder='Company Name' required />
+    <div className='showcase-form card'>
+     <h2>Request a Demo</h2>
+     <form action=''>
+      <div className='form-control'>
+       <input type='text' name='name' placeholder='Name' required />
+      </div>
+      <div className='form-control'>
+       <input type='text' name='company' placeholder='Company Name' required />
+      </div>
+      <div className='form-control'>
+       <input type='email' name='email' placeholder='Email' required />
+      </div>
+      <input type='submit' value='Send' className='btn btn-primary' />
+     </form>
     </div>
-    <div className='form-control'>
-     <input type='email' name='email' placeholder='Email' required />
-    </div>
-    <input type='submit' value='Send' className='btn btn-primary' />
-   </form>
-  </div>
+   </div>
+  </section>
  )
 }
 
