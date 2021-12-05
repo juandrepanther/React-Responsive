47600ffbcfb2d6377ffae50b83d865abd7bfc682
juandrepanther <janis@innergang.com>
2021-12-05 23:45:04 +0200

    added shaddow on clip-path

--- src/pages/ShowCase/ShowCase.js
+++ src/pages/ShowCase/ShowCase.js
@@ -14,21 +14,23 @@ function ShowCase() {
      </p>
      <div className='background-clip-text'>BACKGROUND-CLIP</div>
     </div>
-    <div className='showcase-form card'>
-     <h2>Request a Demo</h2>
-     <form action=''>
-      <div className='form-control'>
-       <input type='text' name='name' placeholder='Name' required />
-      </div>
-      <div className='form-control'>
-       <input type='text' name='company' placeholder='Company Name' required />
-      </div>
-      <div className='form-control'>
-       <input type='email' name='email' placeholder='Email' required />
-      </div>
-      <input type='submit' value='Send' className='btn btn-primary' />
-     </form>
-    </div>
+    <span class='clip-path-shadow-wrapper'>
+     <div className='showcase-form card'>
+      <h2>Request a Demo</h2>
+      <form action=''>
+       <div className='form-control'>
+        <input type='text' name='name' placeholder='Name' required />
+       </div>
+       <div className='form-control'>
+        <input type='text' name='company' placeholder='Company Name' required />
+       </div>
+       <div className='form-control'>
+        <input type='email' name='email' placeholder='Email' required />
+       </div>
+       <input type='submit' value='Send' className='btn btn-primary' />
+      </form>
+     </div>
+    </span>
    </div>
   </section>
  )
